668248b396f9cee97cf46055fece232fd91a5cb4
doruk <dorukbulutinfo@gmail.com>
2022-06-10 19:14:41 +0300

    Updates

---
+++ precache-manifest.bb524e41abdc213e2a938d6d6d143605.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a572587ad81ce4417088",
+    "url": "/Face-Recognition/static/css/main.5aad85d3.chunk.css"
+  },
+  {
+    "revision": "a572587ad81ce4417088",
+    "url": "/Face-Recognition/static/js/main.a572587a.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/Face-Recognition/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "08a280616906280e4bc8",
+    "url": "/Face-Recognition/static/css/2.b90dc385.chunk.css"
+  },
+  {
+    "revision": "08a280616906280e4bc8",
+    "url": "/Face-Recognition/static/js/2.08a28061.chunk.js"
+  },
+  {
+    "revision": "d4108fd9dec938db4aaa",
+    "url": "/Face-Recognition/static/js/runtime~main.d4108fd9.js"
+  },
+  {
+    "revision": "54dde7d9e37d0a622895ebb766de9774",
+    "url": "/Face-Recognition/index.html"
+  }
+];
